70a64b0b3e30ecdea317a338791bbd4c08125b05
huipukui <1137438071@qq.com>
2025-03-14 02:00:57 +0800

    跟随动画延迟难题

--- RNDemo/App.tsx
+++ RNDemo/App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import Anim9 from './src/9/9.9/anim/Anim9';
+import Anim10 from './src/9/9.10/anim/Anim10';
 import {
   SafeAreaView,
   StatusBar,
@@ -23,7 +23,7 @@ function App(): React.JSX.Element {
         barStyle='dark-content'
         backgroundColor='#FFFFFF'
       />
-      <Anim9 />
+      <Anim10 />
     </SafeAreaView>
   );
 }
